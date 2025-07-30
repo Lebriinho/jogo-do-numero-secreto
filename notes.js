a7c5737d2bd4dbf4551f642b8db5f2de184a39be
Arthur Lebre <lebrearthur@hotmail.com>
2025-07-30 16:14:27 -0300

    add notas

---
+++ notes.js
@@ -0,0 +1,31 @@
+/* 
+
+1-PRIMEIROS PASSOS : GIT INIT 
+
+git init
+
+2- FAZER AS ALTERAÇÕES E USAR O COMANDO GIT ADD 
+
+git add . (o ponto é para adicionar todas as alterações!) 
+
+3-REALIZAR O COMMIT!
+
+git commit -m "sua mensagem"
+
+4-CRIAR UMA BRANCH 
+
+git branch -M main (versão principal do cód!)
+
+5-CONEXAO COM O REPOSITÓRIO REMOTO
+
+git remote add origin git@github.com:seunomedeusuario/seu-repositorio.git
+
+6- ENVIAR AS MUDANÇAS PARA O REPOSITÓRIO REMOTO
+
+git push
+*/
+ 
+/* PARA CADASTRAR EMAIL E NOME DE USUARIO!!
+
+git config --global user.email "email do usuario"
+git config --global user.name "nome do usuario" */
